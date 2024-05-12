38b8359c55ad39df07c784959e422d67321746ee
devtruongson <truongsonpt.80@gmail.com>
2024-05-12 17:57:45 +0700

    update Http Response

---
+++ src/utils/Response.ts
@@ -0,0 +1,17 @@
+import httpStatus, { HttpStatus } from 'http-status';
+
+export const ResponseHandler = <T>(
+    HttpStatus: HttpStatus,
+    data: T,
+    msg: string,
+): {
+    code: HttpStatus;
+    data: T;
+    msg: string;
+} => {
+    return {
+        code: HttpStatus,
+        data,
+        msg,
+    };
+};
