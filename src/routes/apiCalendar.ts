cb43012e89a6d2afba8f4580cc0819d4902cabd8
Ngô Ngọc Văn <vanvanvan23062003@gmail.com>
2024-05-18 18:52:38 +0700

    update

--- src/routes/apiCalendar.ts
+++ src/routes/apiCalendar.ts
@@ -10,12 +10,8 @@ const initCalenderRouter = (app: Express) => {
     router.get('/book/:id', handleCheckTokenUserInSystem, calendarController.getCalendarTeacher);
     router.get('/', handleCheckTokenUserInSystem, calendarController.getCalendar);
     router.get('/student', handleCheckTokenUser, calendarController.getCalendarForStudent);
-<<<<<<< HEAD
-    router.get('/book-for-student', handleCheckTokenAdmin, calendarController.bookCalendarForStudent);
     router.post('/student-booking', handleCheckTokenUser, calendarController.handleStudentCreateBooking);
-=======
     router.post('/book-for-student', handleCheckTokenAdmin, calendarController.bookCalendarForStudent);
->>>>>>> ea6b50578591ff2dd078729190f7710bb3ce4072
 
     return app.use('/v1/calendar', router);
 };
