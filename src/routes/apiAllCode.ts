5f7a0e04122354966b2efc9fdb0236a7ac49d0d2
devtruongson <truongsonpt.80@gmail.com>
2024-05-19 17:37:59 +0700

    update

--- src/routes/apiAllCode.ts
+++ src/routes/apiAllCode.ts
@@ -6,7 +6,7 @@ const router = express.Router();
 
 const initApiAllCode = (app: Express) => {
     router.post('/create', handleCheckTokenAdmin, allCodeController.createAllCode);
-    router.get('/:type', handleCheckTokenUserInSystem, allCodeController.getAllCodeByType);
+    router.get('/:type', allCodeController.getAllCodeByType);
 
     return app.use('/v1/all-code', router);
 };
--- src/server.ts
+++ src/server.ts
@@ -3,9 +3,16 @@ import compression from 'compression';
 import connectDB from './configs/connectDB';
 import initApiRoutes from './routes/api';
 import bodyParser from 'body-parser';
+import cors from 'cors';
 
 const app = express();
 app.use(compression());
+
+const corsOption = {
+    origin: ['http://localhost:4000', 'http://localhost:3000', 'http://localhost:5173'], //origin from where you requesting
+    credentials: true,
+};
+app.use(cors(corsOption));
 
 app.use(bodyParser.json({ limit: '50mb' }));
 app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
